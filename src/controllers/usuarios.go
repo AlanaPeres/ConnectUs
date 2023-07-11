932eab346bdcdfdf23724bf970728419d5b719d2
Alana Peres <alanalimaperes@gmail.com>
2023-07-11 18:11:48 -0300

    Adição de métodos de validação e formatação para a estrutura Usuario

--- src/controllers/usuarios.go
+++ src/controllers/usuarios.go
@@ -12,6 +12,7 @@ import (
 
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 	corpoRequest, erro := ioutil.ReadAll(r.Body)
+
 	if erro != nil {
 		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
 		return
@@ -20,6 +21,11 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 	var usuario models.Usuario
 	if erro = json.Unmarshal(corpoRequest, &usuario); erro != nil {
 		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	if erro = usuario.Preparar(); erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
 		return
 	}
 
--- src/models/usuario.go
+++ src/models/usuario.go
@@ -1,8 +1,12 @@
 package models //nesse pacote é armazenado as entidades de usuário e publicações e os métodos q eles podem ter
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
-//Usuario representa um usuário que utiliza a rede social
+// Usuario representa um usuário que utiliza a rede social
 type Usuario struct {
 	ID       uint64    `json: "id,omitempty"` //omitempty, se o campo estiver vazio ele não vai ser passado para o json, ele vai omitir
 	Nome     string    `json: "nome,omitempty"`
@@ -10,4 +14,35 @@ type Usuario struct {
 	Email    string    `json: "email,omitempty"`
 	Senha    string    `json: "senha,omitempty"`
 	CriadoEm time.Time `json: "CriadoEm,omitempty"`
+}
+
+func (usuario *Usuario) Preparar() error {
+	if erro := usuario.validar(); erro != nil {
+		return erro
+	}
+
+	usuario.formatar()
+	return nil
+}
+
+func (usuario Usuario) validar() error {
+	if usuario.Nome == "" {
+		return errors.New("O nome é obrigatório e não pode estar em branco")
+	}
+	if usuario.Nick == "" {
+		return errors.New("O nick é obrigatório e não pode estar em branco")
+	}
+	if usuario.Email == "" {
+		return errors.New("O e-mail é obrigatório e não pode estar em branco")
+	}
+	if usuario.Senha == "" {
+		return errors.New("O senha é obrigatório e não pode estar em branco")
+	}
+	return nil
+}
+
+func (usuario *Usuario) formatar() {
+	usuario.Nome = strings.TrimSpace(usuario.Nome)
+	usuario.Nick = strings.TrimSpace(usuario.Nick)
+	usuario.Email = strings.TrimSpace(usuario.Email)
 }
