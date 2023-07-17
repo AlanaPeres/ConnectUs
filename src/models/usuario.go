37b069ce2a14c69a90f0945cf4430b6cf4d6d533
Alana Peres <alanalimaperes@gmail.com>
2023-07-17 18:19:17 -0300

    Validando formato do e-mail

--- src/models/usuario.go
+++ src/models/usuario.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"strings"
 	"time"
+
+	"github.com/badoux/checkmail"
 )
 
 // Usuario representa um usuário que utiliza a rede social
@@ -34,6 +36,10 @@ func (usuario Usuario) validar(etapa string) error {
 	}
 	if usuario.Email == "" {
 		return errors.New("O e-mail é obrigatório e não pode estar em branco")
+	}
+
+	if erro := checkmail.ValidateFormat(usuario.Email); erro != nil {
+		return errors.New("O e-mail inserido é inválido")
 	}
 	if etapa == "cadastro" && usuario.Senha == "" {
 		return errors.New("O senha é obrigatório e não pode estar em branco")
