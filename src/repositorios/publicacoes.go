4f42c9f56ea930bb1a84102d3796ce94c809463e
Alana Peres <alanalimaperes@gmail.com>
2023-09-04 17:36:57 -0300

    Implementando função para deletar uma publição

--- src/repositorios/publicacoes.go
+++ src/repositorios/publicacoes.go
@@ -107,3 +107,17 @@ func (repositorio Publicacoes) Atualizar(publicacaoID uint64, publicacao models.
 	return nil
 
 }
+
+func (repositorio Publicacoes) Deletar(publicacaoID uint64) error {
+	statement, erro := repositorio.db.Prepare("delete from publicacoes where id = ?")
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(publicacaoID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
