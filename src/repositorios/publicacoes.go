f69c79e12ae9405fc1d4aca3fd2fc4e082d08176
Alana Peres <alanalimaperes@gmail.com>
2023-09-04 17:24:25 -0300

    Implementando função para alterar uma publicação

--- src/repositorios/publicacoes.go
+++ src/repositorios/publicacoes.go
@@ -91,3 +91,19 @@ func (repositorio Publicacoes) Buscar(usuarioID uint64) ([]models.Publicacao, er
 
 	return publicacoes, nil
 }
+
+// Atualizar altera os dados de uma publicacao no banco de dados
+func (repositorio Publicacoes) Atualizar(publicacaoID uint64, publicacao models.Publicacao) error {
+	statement, erro := repositorio.db.Prepare("update publicacoes set titulo = ?, conteudo = ? where id = ?")
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(publicacao.Titulo, publicacao.Conteudo, publicacaoID); erro != nil {
+		return erro
+	}
+
+	return nil
+
+}
